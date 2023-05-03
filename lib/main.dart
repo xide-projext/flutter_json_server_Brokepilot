1eef46e28c413f7194ab12a80aa80289406f3960
Brokepilot <minjunk2329@gmail.com>
2023-05-03 12:00:57 +0900

    Yeet

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  const jsonUrl = "webtoon";
+  var toons = [];
+  try{
+    var response = await get(Uri.http("localhost:3000", jsonUrl));
+    print(response.body);
+    toons = json.decode(response.body);
+  }catch(e){
+    print(e);
+  }
+  runApp(MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(title: const Text("내폰내툰")),
+          body: Row(
+              children: toons
+                  .map(
+                    (item) => Column(children: [
+                      Image.network(item['image']!),
+                      Text(item['title']!)
+                    ]),
+                  ).toList()
+          ))));
+}
